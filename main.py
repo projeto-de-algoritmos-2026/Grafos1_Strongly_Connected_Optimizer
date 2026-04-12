e17f5207e1a642c615b233129446c6d87babe4ed
Laisczt <laissopaes@gmail.com>
2026-04-12 19:59:09 -0300

    comentários

--- main.py
+++ main.py
@@ -34,16 +34,20 @@ def main():
     with open("in.txt", "r") as file:
         for line in file:
             line = line.strip()
+
             if(line[0] == "#"):#Comentários
                 continue  
+
             args = line.split(" ")
             if(len(args) == 3): # declarando Nó
                 if(Node.find_node_by_name(g, args[0]) != -1):
                     raise ValueError(f"{line}\nTentando criar nó duplicado")
+
                 g.append(Node(args[0], float(args[1]), float(args[2])))
             elif(len(args) == 2): # criando arestas
                 ind1 = Node.find_node_by_name(g, args[0])
                 ind2 = Node.find_node_by_name(g, args[1])
+                
                 if(ind1 == -1):
                     raise ValueError(f"{line}\nO primeiro nó na declaração de aresta não foi encontrado")
                 if(ind2 == -1):
@@ -57,22 +61,23 @@ def main():
         print("-=-=-=-=-=-=-")
     
     
-    sccs = []
+    sccs = []   # Conjunto de SCC's
+    # identifica os SCCS
     while(g):
-        gi = ops.inverted(g)
+        gi = ops.inverted(g)    # grafo inverso
 
-        sni = DFS_visit(gi)
+        sni = DFS_visit(gi)     # encontra um sink node
         sn = g[Node.find_node(g, sni)]
 
         if(dbg.debug and dbg.rich):
             print(f"{sn.name} está em componente sink")
             print("-=-=-=-=-=-=-")
         
-        bfg = BFS(g, sn)
-        bfgi = BFS(gi, sni)
-        scc = ops.intersect(bfg, bfgi)
+        bfg = BFS(g, sn)        # BFS a partir do sink node
+        bfgi = BFS(gi, sni)     # BFS no grafo inverso
+        scc = ops.intersect(bfg, bfgi) # encontra o SCC
         sccs.append(scc)
-        g = ops.minus(g, scc)
+        g = ops.minus(g, scc)   # Subtrai o Scc do grafo
     
     if(dbg.debug):
         cont = 1
